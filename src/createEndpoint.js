c629aece50884b29f92b3c1a972286afae738d9b
Gabin <hello@gabinaureche.com>
2017-06-17 15:49:57 +0200

    Add missing type annotation

--- src/createEndpoint.js
+++ src/createEndpoint.js
@@ -6,8 +6,14 @@ import type { CacheHandler } from './createClient';
 export type Endpoint = (opts: void | Object | string) => Promise<*>;
 const ENDPOINT = 'https://api.gwentapi.com/v0';
 
+type Results = { results: [] };
+type Item = { href: string };
+type Params = { offset?: number, limit?: number };
+type Url = string;
+type Options = Results | Item | Params | Url | {};
+
 function createEndpoint(resource: string, cache: CacheHandler): Endpoint {
-  return function fetch(opts = {}) {
+  return function fetch(opts: Options = {}): Promise<*> {
     if (Array.isArray(opts.results)) {
       return Promise.all(opts.results.map(fetch));
     }
