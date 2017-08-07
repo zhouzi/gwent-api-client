6dd0d9055f24884194f4b5dba55989f1fde912ff
Gabin <hello@gabinaureche.com>
2017-08-07 22:18:22 +0200

    Add support for "If-Modified-Since" header

--- src/request/__tests__/request.js
+++ src/request/__tests__/request.js
@@ -3,6 +3,8 @@ import sinon from 'sinon';
 import request from '../request';
 import { createDefaultCacheHandler } from '../../defaultCacheHandler';
 
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
 test.beforeEach((t) => {
   /* eslint-disable no-param-reassign */
 
@@ -58,15 +60,114 @@ test.serial('should throw with a 500 response', async (t) => {
 
 test.serial('should not make an http request if value is cached', async (t) => {
   await request({
-    getItem: () => Promise.resolve('some value'),
+    getItem: () => Promise.resolve({
+      cachedAt: Date.now(),
+      value: 'some value',
+    }),
   }, '/url');
 
   t.is(global.fetch.callCount, 0);
 });
 
+test.serial('should resolve to cached value', async (t) => {
+  const actual = await request({
+    getItem: () => Promise.resolve({
+      cachedAt: Date.now(),
+      value: 'some value',
+    }),
+  }, '/url');
+  const expected = 'some value';
+
+  t.is(actual, expected);
+});
+
 test.serial('should cache the value', async (t) => {
   await request(t.context.cache, '/url');
   await request(t.context.cache, '/url');
+
+  t.is(global.fetch.callCount, 1);
+});
+
+test.serial('should ignore cached value that is more than 3 days long', async (t) => {
+  await request({
+    getItem: () => Promise.resolve({
+      cachedAt: Date.now() - (4 * ONE_DAY),
+      value: 'some value',
+    }),
+    setItem: () => Promise.resolve({}),
+  }, '/url');
+
+  t.is(global.fetch.callCount, 1);
+});
+
+test.serial('should ignore cached value that doesn\'t have a cachedAt property', async (t) => {
+  await request({
+    getItem: () => Promise.resolve({
+      value: 'some value',
+    }),
+    setItem: () => Promise.resolve({}),
+  }, '/url');
+
+  t.is(global.fetch.callCount, 1);
+});
+
+test.serial('should add If-Modified-Since header', async (t) => {
+  const cachedAt = Date.now() - (4 * ONE_DAY);
+  await request({
+    getItem: () => Promise.resolve({
+      cachedAt,
+      value: 'some value',
+    }),
+    setItem: () => Promise.resolve({}),
+  }, '/url');
+
+  t.deepEqual(global.fetch.lastCall.args[1].headers, {
+    'If-Modified-Since': new Date(cachedAt).toUTCString(),
+  });
+});
+
+test.serial('should resolve to cached value if not modified', async (t) => {
+  global.fetch = sinon
+    .stub()
+    .resolves({
+      status: 304,
+    });
+
+  const actual = await request({
+    getItem: () => Promise.resolve({
+      cachedAt: Date.now() - (4 * ONE_DAY),
+      value: 'cached value',
+    }),
+    setItem: () => Promise.resolve({}),
+  }, '/url');
+  const expected = 'cached value';
+
+  t.is(actual, expected);
+});
+
+test.serial('should update cached value if not modified', async (t) => {
+  global.fetch = sinon
+    .stub()
+    .resolves({
+      status: 304,
+    });
+
+  const values = {
+    '/url': {
+      cachedAt: Date.now() - (4 * ONE_DAY),
+      value: 'cached value',
+    },
+  };
+  const cache = {
+    getItem: key => Promise.resolve(values[key]),
+    setItem: (key, value) => {
+      values[key] = value;
+      return Promise.resolve(values[key]);
+    },
+  };
+
+  await request(cache, '/url');
+  await request(cache, '/url');
 
   t.is(global.fetch.callCount, 1);
 });
--- src/request/request.js
+++ src/request/request.js
@@ -4,6 +4,28 @@
 import queryString from 'query-string';
 import type { CacheHandler } from '../types';
 
+const ONE_DAY = 1000 * 60 * 60 * 24;
+const MAX_DAYS_CACHE = 3;
+
+function getCached(cached) {
+  if (cached == null) {
+    return null;
+  }
+
+  if (cached.cachedAt == null) {
+    return null;
+  }
+
+  return cached;
+}
+
+function setCached(value) {
+  return {
+    cachedAt: Date.now(),
+    value,
+  };
+}
+
 function request(
   cache: CacheHandler,
   url: string,
@@ -14,21 +36,38 @@ function request(
       ? `${url}?${queryString.stringify(parameters)}`
       : url;
 
-  return cache.getItem(finalURL).then((value) => {
-    if (value != null) {
-      return Promise.resolve(value);
+  return cache.getItem(finalURL).then(getCached).then((cached) => {
+    if (cached != null) {
+      const daysSinceCached = (Date.now() - cached.cachedAt) / ONE_DAY;
+
+      if (daysSinceCached <= MAX_DAYS_CACHE) {
+        return Promise.resolve(cached.value);
+      }
     }
 
-    return fetch(finalURL)
+    const headers = {};
+
+    if (cached != null) {
+      headers['If-Modified-Since'] = new Date(cached.cachedAt).toUTCString();
+    }
+
+    return fetch(finalURL, { headers })
       .then((response) => {
+        if (cached != null && response.status === 304) {
+          return Promise.resolve(cached.value);
+        }
+
         if (response.status >= 200 && response.status < 300) {
-          return response
-            .json()
-            .then(json => cache.setItem(finalURL, json));
+          return response.json();
         }
 
         throw new Error(response.statusText);
-      });
+      })
+      .then(json =>
+        cache
+          .setItem(finalURL, setCached(json))
+          .then(() => json),
+      );
   });
 }
 
