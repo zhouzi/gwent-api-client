056f9f364629eb110a9be65bca112b2538d19b8f
Gabin <hello@gabinaureche.com>
2017-08-07 22:37:52 +0200

    Add .map() to limit parallel requests

--- src/createClient.js
+++ src/createClient.js
@@ -1,10 +1,34 @@
 /* @flow */
 
+import parallelLimit from 'async.parallellimit';
 import defaultCacheHandler from './defaultCacheHandler';
 import { requestList, requestOne } from './request';
 import type { Client, ClientOptions } from './types';
 
 const APIRootURL = 'https://api.gwentapi.com/v0';
+const MAX_PARALLEL_REQUESTS = 30;
+
+function map(items, request) {
+  const tasks = items.map(item =>
+      callback =>
+        request(item)
+          .then(json => callback(null, json))
+          .catch(err => callback(err)),
+  );
+  return new Promise((resolve, reject) => {
+    parallelLimit(
+      tasks,
+      MAX_PARALLEL_REQUESTS,
+      (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(results);
+      },
+    );
+  });
+}
 
 function getURL(resource: string): string {
   return `${APIRootURL}/${resource}/`;
@@ -24,7 +48,7 @@ function createClient(options: ClientOptions = { cache: defaultCacheHandler }): 
       list: requestList.bind(null, options.cache, getURL(resources[resource])),
       one: requestOne.bind(null, options.cache),
     },
-  }), {});
+  }), { map });
 }
 
 export default createClient;
--- src/types.js
+++ src/types.js
@@ -1,19 +1,12 @@
 /* @flow */
 
-import type { requestList, requestOne } from './request';
-
 export type CacheHandler = {
   getItem: (key: string) => Promise<*>,
   setItem: (key: string, value: *) => Promise<*>,
 };
 
 export type Client = {
-  [string]: {
-    [string]: {
-      list: requestList,
-      one: requestOne,
-    },
-  },
+  [string]: *,
 };
 
 export type ClientOptions = {
