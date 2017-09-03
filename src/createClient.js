b1fe7b5a0598cb7faf1d0614bda5a8ad9d73e5a2
Gabin <hello@gabinaureche.com>
2017-09-03 10:20:24 +0200

    Remove .map()

--- src/createClient.js
+++ src/createClient.js
@@ -1,31 +1,9 @@
 /* @flow */
 
-import parallelLimit from 'async.parallellimit';
 import { requestList, requestOne } from './request';
 import type { Client } from './types';
 
 const APIRootURL = 'https://api.gwentapi.com/v0';
-const MAX_PARALLEL_REQUESTS = 30;
-
-function map(items, request) {
-    console.warn(
-        'gwent-api-client: .map() is deprecated and will be removed in 3.0.0'
-    );
-    const tasks = items.map(item => callback =>
-        request(item)
-            .then(json => callback(null, json))
-            .catch(err => callback(err))
-    );
-    return new Promise((resolve, reject) => {
-        parallelLimit(tasks, MAX_PARALLEL_REQUESTS, (err, results) => {
-            if (err) {
-                return reject(err);
-            }
-
-            return resolve(results);
-        });
-    });
-}
 
 function getURL(resource: string): string {
     return `${APIRootURL}/${resource}/`;
@@ -60,7 +38,7 @@ function createClient(): Client {
                 [resource]: list
             });
         },
-        { map, one: requestOne }
+        { one: requestOne }
     );
 }
 
